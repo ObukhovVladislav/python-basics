68ba4514b1d02ff0b3cc7d07c007a111908b554b
Vladislav Obv <VladislavObv@example.com>
2020-09-14 12:20:41 +0500

    lesson 3

--- 04.lists.py
+++ 04.lists.py
@@ -8,7 +8,10 @@ lesson_dates = [
 student_marks = [5, 4, 3, 2, 5]
 # len(student_marks) ==
 i = 0
-while i < len(student_marks): # i == 0, 0 < 5 -> True ?
+#while i < len(student_marks): # i == 0, 0 < 5 -> True ?
     # i ->0, 0 < 5 -> True?
-    print(lesson_dates[i], 'оценка', student_marks[i])
-    i += 1
+#    print(lesson_dates[i], 'оценка', student_marks[i])
+#    i += 1
+
+for mark in student_marks:
+    print('оценка', mark)
