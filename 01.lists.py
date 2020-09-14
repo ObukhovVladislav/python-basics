079f816acc9fca73b7c9a2f43ab12bf7dcc01e0f
Vladislav Obv <VladislavObv@example.com>
2020-09-14 11:26:07 +0500

    1lesson

--- 01.lists.py
+++ 01.lists.py
@@ -29,6 +29,10 @@ while True:
 print('Ввод завершен')
 print(student_marks)
 
+mock_student_marks = ['5', '4', '3', '2', '5']
+mock_student_marks = [5, 4, 3, 2, 5]
+student_marks = mock_student_marks
+
 # ['5', '4', '3', '5' '2']
 #   0    1    2    3   4
 i = 0
---
+++ 02.lists.py
@@ -0,0 +1,23 @@
+student_marks = []
+#while True:
+#    mark = input('введите оценку студента:\n')
+#    if mark:
+#        student_marks.append(int(mark))
+#    else:
+#        break
+
+#print('ввод завершен')
+#print(student_marks)
+
+#mock_student_marks = ['5', '4', '3', '2', '5'] # wrong mock
+mock_student_marks = [5, 4, 3, 2, 5] # correct mock
+student_marks = mock_student_marks
+
+# data_processing
+i = 0
+avg_mark = 0
+while i < len(student_marks):
+    avg_mark += int(student_marks[i])
+    i += i + 1
+avg_mark /= len(student_marks)
+print('средний балл', avg_mark)
