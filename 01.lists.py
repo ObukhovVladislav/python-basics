077a14ad91dcc6696d9dc67c6f6720a30c6d2180
Vladislav Obv <VladislavObv@example.com>
2020-09-14 11:04:33 +0500

    1lesson

--- 01.lists.py
+++ 01.lists.py
@@ -29,3 +29,28 @@ while True:
 print('Ввод завершен')
 print(student_marks)
 
+# ['5', '4', '3', '5' '2']
+#   0    1    2    3   4
+i = 0
+avg_mark = 0
+while i < len(student_marks):
+    print(type(avg_mark), type(student_marks[i]))
+    avg_mark = avg_mark + int(student_marks[i])
+    i = i + 1
+print(avg_mark)
+
+# <num> + <num2> - binary operation
+# - <num> - unary operation
+
+i = 0
+avg_mark = 0
+while i < len(student_marks):
+    # print(type(avg_mark), type(student_marks[i]))
+    # avg_mark = avg_mark + int(student_marks[i])
+    print(type(avg_mark), type(student_marks[i]))
+    avg_mark += int(student_marks[i])
+    # i = i + 1
+    i += i + 1
+#avg_mark = avg_mark / len(student_marks)
+avg_mark /= len(student_marks)
+print('средний балл', avg_mark)
