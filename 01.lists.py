bb7b950a6d2be10e9e2f4016ab36fe53bb94359d
Vladislav Obv <VladislavObv@example.com>
2020-09-14 10:27:44 +0500

    1lesson

--- 01.lists.py
+++ 01.lists.py
@@ -18,3 +18,14 @@ print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
 my_var = '12.5'
 print(my_var, type(my_var), id(my_var), sys.getsizeof(my_var))
 
+student_marks = []
+while True:
+    mark = input('Введите оценку студента:\n')
+    if mark: # '' == False, 0== False, [] == False
+        student_marks.append(mark)
+    else:
+        break
+
+print('Ввод завершен')
+print(student_marks)
+
