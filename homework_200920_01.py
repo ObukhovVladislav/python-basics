a17965fe8827cd909a95e0cb437e2408def06915
Vladislav Obv <VladislavObv@example.com>
2020-09-20 19:49:24 +0500

    homework

---
+++ homework_200920_01.py
@@ -0,0 +1,22 @@
+student_marks = []
+while True:
+    mark = input('введите оценку студента:\n')
+    if mark:
+        student_marks.append(int(mark))
+        if int(mark) > 5:
+            print('Введите оценку от 1 до 5')
+            student_marks.remove(int(mark))
+        elif int(mark) == 0:
+            student_marks.remove(int(mark))
+    else:
+        break
+print('Ввод завершён')
+avg_mark = 0
+for mark in student_marks:
+    avg_mark += mark
+avg_mark /= len(student_marks)
+if not student_marks:
+    print('Пусто')
+else:
+    print('Оценки:', student_marks)
+    print('Средний балл', avg_mark)
---
+++ homework_200920_03.py
@@ -0,0 +1,11 @@
+# a = 'И'.center(3, 'М')
+# print(a)
+#
+# b = 'оооо'.count('оо')
+# print(b)
+#
+# c ='100\t200\t300\t400\t500\t600'.expandtabs()
+# print(c)
+
+# d = 'd'.find('FF')
+# print(d)
