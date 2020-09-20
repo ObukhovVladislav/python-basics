9f954f67e7f57b1170abcb9b7a03ead6b9e47d68
Vladislav Obv <VladislavObv@example.com>
2020-09-20 18:31:58 +0500

    string

---
+++ 05.str.py
@@ -0,0 +1,15 @@
+say = 'всем привет!'
+print(say)
+#print(dir(say))
+print(say.islower())
+print(say.isupper())
+
+say_2 = say.upper()
+print(say_2)
+
+# say_3 = say_2.capitalize()
+say_3 = say.capitalize()
+print(say_3)
+
+if say.endswith('!')
+    print('Эмоционально сказано')
