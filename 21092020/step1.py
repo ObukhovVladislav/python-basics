464517f796227118b0890b798586e27b7c9dac4d
Vladislav Obv <VladislavObv@example.com>
2020-09-25 21:12:26 +0500

    lesson

---
+++ 21092020/step1.py
@@ -0,0 +1,54 @@
+# refactoring
+# dry: Do not Repeat Yourself
+# habra-effect -> 404
+# bottleneck
+# brute force
+
+# Первый пример кода прошлого домашнего задания
+# student_marks = []
+# while True:
+#     mark = input('Введите оценку студента:\n')
+# #    mark = int(mark) # int('') -> exception
+#     if mark:
+#         student_marks.append(int(mark))
+#         if int(mark) > 5:
+#             print('Оценка не подходит под категорию оценивания')
+#             student_marks.remove(int(mark))
+#         elif int(mark) < 1:
+#             print('Оценка не подходит под категорию оценивания')
+#             student_marks.remove(int(mark))
+#     else:
+#         break
+# print('Ввод завершён')
+
+# Второй пример кода прошлого домашнего задания
+# student_marks = []
+# while True:
+#     mark = input('Введите оценку студента:\n')
+#     # mark = int(mark) # int('') -> exception
+#     if mark:
+#         mark = int(mark)  # int('5') -> ok
+#         student_marks.append(mark)
+#         if mark > 5:
+#             print('Оценка не подходит под категорию оценивания')
+#             student_marks.remove(mark)
+#         elif mark < 1:
+#             print('Оценка не подходит под категорию оценивания')
+#             student_marks.remove(mark)
+#     else:
+#         break
+# print('Ввод завершён')
+
+
+# 9 mark = '7'
+# 11 True
+#       students_marks = [4, 4, 3, 2, 4, 5, 5, 4, 2]
+# 12 mark = 7
+# 13 students_marks = [4, 4, 3, 2, 4, 5, 5, 4, 2, 7]
+# 14 True
+# 16 student_marks.remove(mark) -> students.marks.remove(7)
+# remove() -> search in list (brute force, linear search)
+# [4, 4, 3, 2, 4, 5, 5, 4, 2, 7], n = len(student_marks) -> task size, n steps
+# [7, 4, 4, 3, 2, 4, 5, 5, 4, 2] -> 1 step, [4, 4, 3, 2, 7, 4, 5, 5, 4, 2] -> n // 2 steps
+# list search complexity = O(n)
+# O(n) - о большое эн
