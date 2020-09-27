6bc487261db743e0153a22dcc7df22abfb9f14af
Vladislav Obv <VladislavObv@example.com>
2020-09-27 16:26:52 +0500

    lesson

--- 21092020/step2.py
+++ 21092020/step2.py
@@ -3,13 +3,28 @@ while True:
     mark = input('введите оценку студента:\n')
     if mark:
         mark = int(mark)
-        if 1 <= mark <= 5:
+        if mark > 5 or mark < 1:
+            print('Оценка не подходит под категорию оценивания')
+        else:
             student_marks.append(mark)
-        elif mark > 5:
-            print("Оценка больше 5")
-        elif mark < 1:
-            print("Оценка меньше 1")
     else:
         break
 
 print('ввод завершен')
+
+
+# student_marks = []
+# while True:
+#     mark = input('введите оценку студента:\n')
+#     if mark:
+#         mark = int(mark)
+#         if 1 <= mark <= 5:
+#             student_marks.append(mark)
+#         elif mark > 5:
+#             print("Оценка больше 5")
+#         elif mark < 1:
+#             print("Оценка меньше 1")
+#     else:
+#         break
+#
+# print('ввод завершен')
