f175bc4dc51d670ed0bbfa8742ff9b3a9a2ced5a
Vladislav Obv <VladislavObv@example.com>
2020-10-04 12:14:26 +0500

    lesson1

--- 28092020/warm_up.py
+++ 28092020/warm_up.py
@@ -20,7 +20,15 @@ row_records = [
     '188.138.60.101 - - [17/May/2015:08:05:49 +0000] "GET /downloads/product_2 HTTP/1.1" 304 0 "-" "Debian APT-HTTP/1.3 (0.9.7.9)"',
 ]
 
-idx = 0
-while idx < len(row_records):
-    print(row_records[idx])
-    idx += 1
+# idx = 0
+# while idx < len(row_records):
+#     print(row_records[idx])
+#     idx += 1
+
+for row_record in row_records:
+    # print(row_record)
+    correct_requests_IP_address = '93.180.71.3'
+    correct_requests_date = '17/May/2015:08:05:32 +0000'
+    requests_IP_address = row_record.split()[0]
+    requests_date = row_record.split()[3]
+    print(requests_IP_address, requests_date)
