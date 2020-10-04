73cb1909a1bb90fe3e75d884fe5d126a037fb395
Vladislav Obv <VladislavObv@example.com>
2020-10-04 22:06:51 +0500

    - readlines() - f.close() - context manager - f.write() - some fixed - try

--- 28092020/step_05.py
+++ 28092020/step_05.py
@@ -5,9 +5,28 @@ parsed_data_file = 'nginx_logs_parsed.csv'
 # init
 parsed_data = []
 
+def row_parse(row):
+    remote_IP_address, row_tail = row.split(maxsplit=1)
+    _, _request_datetime = row_tail.split('[', maxsplit=1)
+    request_datetime, row_tail = _request_datetime.split(']', maxsplit=1)
+
+    _, _request_method, row_tail = row_tail.split('"', maxsplit=2)
+    request_method, requested_resource, _ = _request_method.split(maxsplit=2)
+
+    # prepare result
+    parsed_row = [remote_IP_address, request_datetime, request_method, requested_resource]
+    parsed_row = list(map(str.strip, parsed_row))
+
+    # append result
+    parsed_data.append(parsed_row)
+    # print(parsed_row)
 # parse
 with open(src_data_file, 'r', encoding='utf-8') as f:
     for row in f.read().splitlines():
+        # validation
+        if not row:
+            continue
+
         #row parse
         remote_IP_address, row_tail = row.split(maxsplit=1)
         _, _request_datetime = row_tail.split('[', maxsplit=1)
