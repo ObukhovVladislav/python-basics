e292fd2e5f9b3c43cc243bb615b5d95f2d22b583
Vladislav Obv <VladislavObv@example.com>
2020-10-05 07:55:48 +0500

    - readlines() - f.close() - context manager - f.write() - some fixed - try - def

--- 28092020/step_05.py
+++ 28092020/step_05.py
@@ -5,6 +5,7 @@ parsed_data_file = 'nginx_logs_parsed.csv'
 # init
 parsed_data = []
 
+
 def row_parse(row):
     remote_IP_address, row_tail = row.split(maxsplit=1)
     _, _request_datetime = row_tail.split('[', maxsplit=1)
@@ -15,12 +16,17 @@ def row_parse(row):
 
     # prepare result
     parsed_row = [remote_IP_address, request_datetime, request_method, requested_resource]
-    parsed_row = list(map(str.strip, parsed_row))
+    return list(map(str.strip, parsed_row))
 
-    # append result
-    parsed_data.append(parsed_row)
-    # print(parsed_row)
-# parse
+
+# result = row_parse(
+#     '93.180.71.3 - - [17/May/2015:08:05:32 +0000] "GET /downloads/product_1 HTTP/1.1" 304 0 "-" '
+#     '"Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)"'
+# )
+# assert result == ['93.180.71.3', '17/May/2015:08:05:32 +0000', 'GET', '/downloads/product_1']
+
+
+## parse
 with open(src_data_file, 'r', encoding='utf-8') as f:
     for row in f.read().splitlines():
         # validation
@@ -28,23 +34,14 @@ with open(src_data_file, 'r', encoding='utf-8') as f:
             continue
 
         #row parse
-        remote_IP_address, row_tail = row.split(maxsplit=1)
-        _, _request_datetime = row_tail.split('[', maxsplit=1)
-        request_datetime, row_tail = _request_datetime.split(']', maxsplit=1)
-
-        _, _request_method, row_tail = row_tail.split('"', maxsplit=2)
-        request_method, requested_resource, _ = _request_method.split(maxsplit=2)
-
-        # prepare result
-        parsed_row = [remote_IP_address, request_datetime, request_method, requested_resource]
-        parsed_row = list(map(str.strip, parsed_row))
+        parsed_row = row_parse(row)
 
         # append result
         parsed_data.append(parsed_row)
-        # print(parsed_row)
 
-# print(parsed_data[2])
-# save
+
+## print(parsed_data[2])
+## save
 with open(parsed_data_file, 'w', encoding='utf-8') as f:
     for row in parsed_data:
         # f.write(', '.join(row))  # .csv
