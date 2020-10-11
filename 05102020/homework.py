03847abf81944194a0d4a1d7d1475bf9cc69ce07
Vladislav Obv <VladislavObv@example.com>
2020-10-11 10:50:44 +0500

    homework

---
+++ 05102020/homework.py
@@ -0,0 +1,75 @@
+class Student:
+    def __init__(self):
+        self.name = None
+        self.surname = None
+        self.patronymic = None
+        self.group_number = None
+        self.passport_data = None
+        self.student_card = None
+        self.gradebook = None
+
+
+
+
+class Teacher:
+    def __init__(self):
+        self.name = None
+        self.surname = None
+        self.patronymic = None
+        self.passport_data = None
+        self.subject = None
+
+
+
+
+
+
+class study_group:
+    def __init__(self):
+        self.number = None
+        self.number_of_people = None
+        self.
+        self.
+        self.
+
+
+
+class College:
+    def __init__(self):
+        self.college_name = None
+        self.
+        self.
+        self.
+        self.
+
+
+class Exam:
+    def __init__(self):
+        self.
+        self.
+        self.
+        self.
+        self.
+
+
+class Pupil_in_the_exam:
+    def __init__(self):
+        self.
+        self.
+        self.
+        self.
+        self.
+
+
+
+class Car:
+    def __init__(self):
+        self.engine = None
+        self.color = None
+        self.brand = None
+        self.mark = None
+
+
+
+
+
